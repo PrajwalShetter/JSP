0f6600e3cf4452531feefa9c31f21cbce7da6022
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-18 23:56:06 +0530

    fetching data from the server means form to the console

--- blood_bank_System/src/main/java/com/xworkz/bloodapp/servlet/AccountRegistrationServlet.java
+++ blood_bank_System/src/main/java/com/xworkz/bloodapp/servlet/AccountRegistrationServlet.java
@@ -1,5 +1,7 @@
 package com.xworkz.bloodapp.servlet;
 
+import com.xworkz.bloodapp.dto.UserDto;
+
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -13,6 +15,28 @@ public class AccountRegistrationServlet extends GenericServlet {
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+        String email = req.getParameter("EmailId");
+        String firstname    = req.getParameter("firstname");
+        String lastname = req.getParameter("lastname");
+        String bloodGroup = req.getParameter("bloodGroup");
+        String userId = req.getParameter("UserId");
+        String password = req.getParameter("password");
+        String repeatPassword = req.getParameter("repeatPassword");
+
+
+        UserDto userDto = new UserDto();
+        userDto.setEmail(email);
+        userDto.setFirstName(firstname);
+        userDto.setLastName(lastname);
+        userDto.setUserId(userId);
+        userDto.setPassword(password);
+        userDto.setBloodGroup(bloodGroup);
+        userDto.setRepeatPassword(repeatPassword);
+
+
+
+        System.out.println(userDto);
 
         PrintWriter printWriter = res.getWriter();
         printWriter.println("Thanks for Registration");
