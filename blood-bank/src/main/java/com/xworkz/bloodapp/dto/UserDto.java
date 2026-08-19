3d827e4c95371ce23d80c4e2f58556a71fe80174
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-19 23:56:07 +0530

    storinfg data from form to mysql

---
+++ blood-bank/src/main/java/com/xworkz/bloodapp/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.xworkz.bloodapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserDto {
+
+    private String email;
+    private String firstname;
+    private  String lastname;
+    private String userId;
+    private  String password;
+    private String repeatPassword;
+    private String bloodGroup;
+}
