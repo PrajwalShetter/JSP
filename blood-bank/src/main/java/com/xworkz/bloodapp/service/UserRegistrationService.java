09dfa94f06a4466a45edeaa2d06b5ebd7c8a8aae
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-20 23:50:22 +0530

    adding getBy UserId method

--- blood-bank/src/main/java/com/xworkz/bloodapp/service/UserRegistrationService.java
+++ blood-bank/src/main/java/com/xworkz/bloodapp/service/UserRegistrationService.java
@@ -1,8 +1,10 @@
 package com.xworkz.bloodapp.service;
 
 import com.xworkz.bloodapp.dto.UserDto;
+import com.xworkz.bloodapp.entity.UserEntity;
 
 public interface UserRegistrationService {
 
     boolean saveUser(UserDto user);
+    UserDto getUser(String userId);
 }
