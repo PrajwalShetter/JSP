6abc21ddc8598404301742d48d467eb92584fee9
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-23 13:57:02 +0530

    Keep only selected projects in repository

--- ApexLog/src/main/java/com/xworkz/apexLog/dao/UserRegistrationDao.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.apexLog.dao;
-
-import com.xworkz.apexLog.entity.UserEntity;
-
-public interface UserRegistrationDao {
-    boolean saveUser(UserEntity user);
-    UserEntity getUser(int id);
-}
--- ApexLog/src/main/java/com/xworkz/apexLog/service/UserRegistrationService.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.apexLog.service;
-
-import com.xworkz.apexLog.dto.UserDto;
-
-public interface UserRegistrationService {
-    boolean saveUser(UserDto user);
-    UserDto getUser(int id);
-}
