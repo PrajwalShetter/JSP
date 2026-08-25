c9ed97f01173796ed3034a013d85b679e241ee9c
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-25 23:40:52 +0530

    this is the servlet program of GetAllUsers, and transfering data to the UI pages

---
+++ blood-bank/src/main/java/com/xworkz/bloodapp/servlet/GetAllUserServlet.java
@@ -0,0 +1,37 @@
+package com.xworkz.bloodapp.servlet;
+
+import com.xworkz.bloodapp.dto.UserDto;
+import com.xworkz.bloodapp.service.UserRegistrationService;
+import com.xworkz.bloodapp.service.impl.UserRegistrationServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/getUsers")
+public class GetAllUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        UserRegistrationService userRegistrationService = new UserRegistrationServiceImpl();
+        List<UserDto> users = userRegistrationService.getAllUser();
+
+        for (UserDto user : users){
+            System.out.println(user);
+        }
+
+        req.setAttribute("dto", users);
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("viewUsers.jsp");
+        requestDispatcher.forward(req,resp);
+
+
+
+
+    }
+}
