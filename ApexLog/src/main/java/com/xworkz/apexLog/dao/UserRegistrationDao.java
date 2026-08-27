31a9a2e3f2fb137be29539a83668befe3cf41671
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-27 23:59:30 +0530

    DAO files are responsible for inserting and fetching data from DB

---
+++ ApexLog/src/main/java/com/xworkz/apexLog/dao/UserRegistrationDao.java
@@ -0,0 +1,11 @@
+package com.xworkz.apexLog.dao;
+
+import com.xworkz.apexLog.entity.UserEntity;
+
+import java.util.List;
+
+public interface UserRegistrationDao {
+    boolean saveUser(UserEntity user);
+    UserEntity getUser(int id);
+    List<UserEntity> getAllUsers();
+}
---
+++ ApexLog/src/main/java/com/xworkz/apexLog/dao/impl/UserRegistrationDaoImpl.java
@@ -0,0 +1,53 @@
+package com.xworkz.apexLog.dao.impl;
+
+import com.xworkz.apexLog.dao.UserRegistrationDao;
+import com.xworkz.apexLog.entity.UserEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Collections;
+import java.util.List;
+
+public class UserRegistrationDaoImpl implements UserRegistrationDao {
+
+    private Configuration configuration = new Configuration();
+    private SessionFactory sessionFactory;
+
+    public UserRegistrationDaoImpl() {
+        configuration.configure();
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    @Override
+    public boolean saveUser(UserEntity user) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(user);
+
+        transaction.commit();
+        session.close();
+        return true;
+    }
+
+    @Override
+    public UserEntity getUser(int id) {
+        Session session = sessionFactory.openSession();
+        UserEntity user = session.get(UserEntity.class,id);
+        session.close();
+        sessionFactory.close();
+
+        return user ;
+    }
+
+    @Override
+    public List<UserEntity> getAllUsers() {
+        Session session = sessionFactory.openSession();
+        List<UserEntity> userEntities = session.createQuery("from UserEntity", UserEntity.class).getResultList();
+        session.close();
+        sessionFactory.close();
+        return userEntities;
+    }
+}
