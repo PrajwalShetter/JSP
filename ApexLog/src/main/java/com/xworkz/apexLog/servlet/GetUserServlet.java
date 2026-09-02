8ed1b76367aa030ebaab51e8829592e84cfa7fde
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-09-02 23:27:13 +0530

    Changing forward path to another servlet

--- ApexLog/src/main/java/com/xworkz/apexLog/servlet/GetUserServlet.java
+++ ApexLog/src/main/java/com/xworkz/apexLog/servlet/GetUserServlet.java
@@ -6,21 +6,26 @@ import com.xworkz.apexLog.service.impl.UserRegistrationServiceImpl;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/getUser")
-public class GetUserServlet extends GenericServlet {
+public class GetUserServlet extends HttpServlet {
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 
         UserRegistrationService userRegistrationService = new UserRegistrationServiceImpl();
         String id = req.getParameter("id");
         UserDto userDto =userRegistrationService.getUser(Integer.parseInt(id));
 
         req.setAttribute("dto",userDto);
-        RequestDispatcher requestDispatcher =req.getRequestDispatcher("/getUser.jsp");
-        requestDispatcher.forward(req, res);
-
+        RequestDispatcher requestDispatcher =req.getRequestDispatcher("/updateAccount.jsp");
+        requestDispatcher.forward(req, resp);
     }
+
+
 }
