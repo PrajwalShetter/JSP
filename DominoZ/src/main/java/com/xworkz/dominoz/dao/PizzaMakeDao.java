2585dac0b5c6ae34f2519e75f36e3ac4a57361a9
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-09-10 23:44:44 +0530

    Dao interface for PizzaMake

---
+++ DominoZ/src/main/java/com/xworkz/dominoz/dao/PizzaMakeDao.java
@@ -0,0 +1,8 @@
+package com.xworkz.dominoz.dao;
+
+import com.xworkz.dominoz.entity.PizzaMakeEntity;
+
+public interface PizzaMakeDao {
+
+    boolean savePizzaMaker(PizzaMakeEntity pizzaMakeEntity);
+}
