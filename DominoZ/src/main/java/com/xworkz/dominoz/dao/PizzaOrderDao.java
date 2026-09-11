d6dde94c522fff742c9f8ca1e06f61b3a5c6e68b
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-09-11 23:48:30 +0530

    Order Dao interface for pizzaOrder

---
+++ DominoZ/src/main/java/com/xworkz/dominoz/dao/PizzaOrderDao.java
@@ -0,0 +1,8 @@
+package com.xworkz.dominoz.dao;
+
+import com.xworkz.dominoz.entity.PizzaOrderEntity;
+
+public interface PizzaOrderDao {
+
+    boolean saveOrder(PizzaOrderEntity pizzaOrderEntity);
+}
